53322ab74159566ff5ef2939f0473327b17e2b62
shamilmelis <akastramormoley1st@mail.ru>
2024-02-10 21:33:39 +0600

    upd14: test

--- js/main.js
+++ js/main.js
@@ -54,6 +54,70 @@ const catalogPopup = document.querySelector('.nav_routes')
 catalogPopup.addEventListener('mouseenter', () => {
     const el = document.querySelector('.catalog_popup_box')
     el.style.display = "flex"
+    const routes = document.getElementsByClassName('main_route_link')
+    const box = document.getElementsByClassName('popup_box_routes_box')
+    for (let i = 0; i < routes.length; i++) {
+        routes[i].addEventListener('mouseenter', () => {
+            for (let x = 0 ; x < box.length; x++) {
+                switch (routes[i].innerText) {
+                    case 'Бытовая химия':
+                        box[0].style.display = "flex"
+                        box[1].style.display = "none"
+                        box[2].style.display = "none"
+                        box[3].style.display = "none"
+                        box[4].style.display = "none"
+                        box[5].style.display = "none"
+                        box[6].style.display = "none"
+                        break
+                    case 'Профессиональный клининг':
+                        box[0].style.display = "none"
+                        box[1].style.display = "flex"
+                        box[2].style.display = "none"
+                        box[3].style.display = "none"
+                        box[4].style.display = "none"
+                        box[5].style.display = "none"
+                        box[6].style.display = "none"
+                        break
+                    case 'Промышленная химия':
+                        box[0].style.display = "none"
+                        box[1].style.display = "none"
+                        box[2].style.display = "flex"
+                        box[3].style.display = "none"
+                        box[4].style.display = "none"
+                        box[5].style.display = "none"
+                        box[6].style.display = "none"
+                        break
+                    case 'Автохимия':
+                        box[0].style.display = "none"
+                        box[1].style.display = "none"
+                        box[2].style.display = "none"
+                        box[3].style.display = "flex"
+                        box[4].style.display = "none"
+                        box[5].style.display = "none"
+                        box[6].style.display = "none"
+                        break
+                    case 'Уход за собой':
+                        box[0].style.display = "none"
+                        box[1].style.display = "none"
+                        box[2].style.display = "none"
+                        box[3].style.display = "none"
+                        box[4].style.display = "flex"
+                        box[5].style.display = "none"
+                        box[6].style.display = "none"
+                        break
+                    case 'Продукция для гостиниц':
+                        box[0].style.display = "none"
+                        box[1].style.display = "none"
+                        box[2].style.display = "none"
+                        box[3].style.display = "none"
+                        box[4].style.display = "none"
+                        box[5].style.display = "flex"
+                        box[6].style.display = "none"
+                        break
+                }
+            }
+        })
+    }
     el.addEventListener('mouseleave', () => {
         el.style.display = "none"
     })
